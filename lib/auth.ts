d2d088a9861e02d00d0d3e2ae0e8d02a16ebc11a
Hubert Grzesiak <vTenn3R@wp.pl>
2024-05-28 19:53:21 +0200

    user types fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,41 @@
 import { auth } from "@/auth";
 
-export const currentUser = async () => {
-  const session = await auth();
 
-  return session?.user;
+import { PrismaClient, User as PrismaUser } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export type User = PrismaUser & {
+  emailVerified: Date | null;
+  password: string | null;
+  twoFactorConfirmationId: string | null;
+  conversationIds: string[];
+  seenMessageIds: string[];
 };
 
-export const currentRole = async () => {
+export const currentUser = async (): Promise<User | null> => {
   const session = await auth();
 
+  if (session?.user) {
+    const { id } = session.user;
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (user) {
+      return {
+        ...user,
+        emailVerified: user.emailVerified,
+        password: user.password,
+        twoFactorConfirmationId: user.twoFactorConfirmationId,
+        conversationIds: user.conversationIds || [],
+        seenMessageIds: user.seenMessageIds || [],
+      };
+    }
+  }
+
+  return null;
+};
+
+export const currentRole = async (): Promise<string | undefined> => {
+  const session = await auth();
   return session?.user?.role;
 };
