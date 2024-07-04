408c3cda01ecdc9e98451d43ddae2322e4d993e4
Hubert Grzesiak <vTenn3R@wp.pl>
2024-07-04 20:30:37 +0200

    stripe_webhook_secret changed

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,23 +1,20 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { headers } from "next/headers";
 import { createReservation } from "@/lib/actions/reservation/transaction.action";
 
 export async function POST(request: Request) {
   const body = await request.text();
 
-  const sig = request.headers.get("stripe-signature") as string;
+  const signature = headers().get("Stripe-Signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
-  console.log("Received Stripe webhook with body:", body);
-  console.log("Stripe signature:", sig);
-  console.log("Stripe webhook secret:", endpointSecret);
-
-  const stripe = new Stripe(process.env.STRIPE_KEY!);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(body, signature, endpointSecret);
     console.log("Webhook event constructed successfully:", event);
   } catch (err) {
     return NextResponse.json(
