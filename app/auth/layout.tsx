707d4f6bc08db08f3dd4b89805b3f476189e0fc3
Hubert Grzesiak <vTenn3R@wp.pl>
2024-07-10 21:40:36 +0200

    wip

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,12 +1,8 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div
-      className="
-          h-full flex items-center justify-center min-h-[90vh]
-          bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-neutral-900 via-neutral-600 to-neutral-900
-        ">
+    <main className="background-login flex h-full items-center justify-center">
       {children}
-    </div>
+    </main>
   );
 };
 
